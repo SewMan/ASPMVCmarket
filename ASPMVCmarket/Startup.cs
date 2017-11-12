962d494608b794bbd47674de6f23773a0568d246
Łaśkens <sewelas667@student.polsl.pl>
2017-11-12 13:11:43 +0100

    Init.

---
+++ ASPMVCmarket/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASPMVCmarket.Startup))]
+namespace ASPMVCmarket
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
