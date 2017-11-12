450e347afb8ec15ae86198d3b0038b09807594f3
Łaśkens <sewelas667@student.polsl.pl>
2017-11-12 14:16:28 +0100

    Added: Car Model, Random View, CarController. First build.

---
+++ ASPMVCmarket/Controllers/CarController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ASPMVCmarket.Models;
+
+namespace ASPMVCmarket.Controllers
+{
+    public class CarController : Controller
+    {
+        // GET: Car/Random
+        public ActionResult Random()
+        {
+            var car = new Car() { Name = "Clio III" };
+            return View(car);
+        }
+    }
+}
