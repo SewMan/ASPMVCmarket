1d526a4c50d059370b1d59083b2ec5ab2f6be2a6
Łaśkens <sewelas667@student.polsl.pl>
2017-11-28 23:57:36 +0100

    Added: -CarDetails view -Migrations
    Changed:
    -Car model
    -Customer model
    -IdentityModels (it's database model)
    -MembershipType model
    -Car controller
    -Customer controller

---
+++ ASPMVCmarket/Migrations/201711282056428_CarModelUpdate.cs
@@ -0,0 +1,29 @@
+namespace ASPMVCmarket.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CarModelUpdate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Cars",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        ReleaseData = c.DateTime(nullable: false),
+                        AmountInStock = c.Int(nullable: false),
+                        Description = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Cars");
+        }
+    }
+}
