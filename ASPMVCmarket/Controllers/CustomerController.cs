d36f3546d79aaf8ecf306691906eea0cbffb265f
Łaśkens <sewelas667@student.polsl.pl>
2018-01-03 01:12:40 +0100

    WIP, validation added- required some code cleaning.

--- ASPMVCmarket/Controllers/CustomerController.cs
+++ ASPMVCmarket/Controllers/CustomerController.cs
@@ -50,6 +50,7 @@ namespace ASPMVCmarket.Controllers
             var membershipTypes = _context.MembershipTypes.ToList();
             var viewModel = new CustomerFormViewModel()
             {
+                Customer = new Customer(),
                 MembershipTypes = membershipTypes
             };
 
@@ -58,8 +59,20 @@ namespace ASPMVCmarket.Controllers
 
         //Save new customer to the database
         [HttpPost] //<- can only be called by HttpPost not HttpGet
+        [ValidateAntiForgeryToken] //<- Token for Cross-Site Request Forgery (CSRF) prevention
         public ActionResult Save(Customer customer)
         {
+            // Validation
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new CustomerFormViewModel
+                {
+                    Customer = customer,
+                    MembershipTypes = _context.MembershipTypes.ToList()
+                };
+
+                return View("CustomerForm", viewModel);
+            }
 
             if (customer.Id == 0)
                 _context.Customers.Add(customer);
