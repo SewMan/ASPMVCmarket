b6001332a96472773e7c80105656830ae372c529
Łaśkens <sewelas667@student.polsl.pl>
2018-01-06 01:49:33 +0100

    Added customer's API.

---
+++ ASPMVCmarket/App_Start/MappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ASPMVCmarket.Dtos;
+using ASPMVCmarket.Models;
+using AutoMapper;
+
+namespace ASPMVCmarket.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            Mapper.CreateMap<Customer, CustomerDto>();
+            Mapper.CreateMap<CustomerDto, Customer>();
+        }
+    }
+}
